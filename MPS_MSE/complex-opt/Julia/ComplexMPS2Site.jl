2b682823eb798b3fc127ada78a0a82ea86ab62ee
jmoo2880 <jmoo2880@uni.sydney.edu.au>
2024-03-05 10:08:47 +1100

    more work on complex 2 site MPS

--- MPS_MSE/complex-opt/Julia/ComplexMPS2Site.jl
+++ MPS_MSE/complex-opt/Julia/ComplexMPS2Site.jl
@@ -3,6 +3,7 @@ using ITensors
 using Random
 using Plots 
 using Base.Threads
+using Distributions
 
 Random.seed!(42)
 
@@ -32,11 +33,47 @@ function generate_training_data(samples_per_class::Int)
 
 end
 
+function generate_mv_training_data(samples_per_class::Int)
+    mean_A = [0.3, 0.7] # upper left of unit square
+    cov_A = [0.02 0.01; 0.01 0.02]
+    dist_A = MvNormal(mean_A, cov_A)
+
+    mean_B = [0.7, 0.3]
+    cov_B = [0.01 0.005; 0.005 0.01]
+    dist_B = MvNormal(mean_B, cov_B)
+
+    samples_A = Matrix{Float64}(undef, 0, 2)  # Initialize as an empty 0x2 matrix
+    samples_B = Matrix{Float64}(undef, 0, 2)  # Initialize as an empty 0x2 matrix
+    
+    while size(samples_A, 1) < samples_per_class
+        temp_samples_A = transpose(rand(dist_A, samples_per_class - size(samples_A, 1)))
+        filtered_samples_A = filter(x -> 0 <= x[1] <= 1 && 0 <= x[2] <= 1, eachrow(temp_samples_A))
+        for sample in filtered_samples_A
+            samples_A = vcat(samples_A, reshape(sample, 1, length(sample)))  # Reshape each sample to a 1x2 row
+        end
+    end
+    
+    while size(samples_B, 1) < samples_per_class
+        temp_samples_B = transpose(rand(dist_B, samples_per_class - size(samples_B, 1)))
+        filtered_samples_B = filter(x -> 0 <= x[1] <= 1 && 0 <= x[2] <= 1, eachrow(temp_samples_B))
+        for sample in filtered_samples_B
+            samples_B = vcat(samples_B, reshape(sample, 1, length(sample)))  # Reshape each sample to a 1x2 row
+        end
+    end
+
+    all_samples = vcat(samples_A, samples_B)
+    all_labels = Int.(vcat(zeros(size(samples_A)[1]), ones(size(samples_B)[1]))) # maximise overlap with class B
+    
+    return all_samples, all_labels
+
+end
+
+
 function make_prediction(mps, ps)
     # make a prediction a single product state
     res = 1
     for i in eachindex(mps)
-        res *= mps[i] * ps.pstate[i]
+        res *= mps[i] * conj(ps.pstate[i])
     end
     yhat = abs(res[]) # modulus of overlap
     # binary classifier so use a threshold of 0.5 to make predictions
@@ -77,7 +114,7 @@ function dataset_to_product_state(dataset::Matrix, labels::Vector, sites::Vector
 
     all_product_states = Vector{PState}(undef, size(dataset)[1])
     for p=1:length(all_product_states)
-        sample_pstate = DataToProductState(dataset[p, :], sites)
+        sample_pstate = sample_to_product_state(dataset[p, :], sites)
         sample_label = labels[p]
         product_state = PState(sample_pstate, sample_label)
         all_product_states[p] = product_state
@@ -136,12 +173,12 @@ function loss_and_gradient_dataset(mps, all_pstates)
 end
 
 
-all_samples, all_labels = generate_training_data(200)
+all_samples, all_labels = generate_mv_training_data(500)
 all_product_states = dataset_to_product_state(all_samples, all_labels, sites)
 shuffle!(all_product_states)
 
 num_steps = 500
-lr = 0.5
+lr = 0.8
 init_loss = loss_per_dataset(mps, all_product_states)
 init_acc = accuracy_per_dataset(mps, all_product_states)
 println("Initial loss: $init_loss | initial acc: $init_acc")
@@ -168,4 +205,138 @@ for step=1:num_steps
     push!(running_loss, new_loss)
     println("Step $step, loss = $new_loss")
 end
+println("Final test accuracy: $(accuracy_per_dataset(mps, all_product_states))")
+all_test_samples, all_test_labels = generate_mv_training_data(500)
+all_test_product_states = dataset_to_product_state(all_test_samples, all_test_labels, sites)
+println("Test accuracy: $(accuracy_per_dataset(mps, all_test_product_states))")
+
+# not efficient, I know... 
+function SampleMPS(num_samples)
+    # do site 1 first
+    samples = Matrix{Float64}(undef, num_samples, 2)
+    xs = collect(0:0.01:1)
+    states = [complex_feature_map(x) for x in xs]
+    mps_copy_site_1 = deepcopy(mps)
+    orthogonalize!(mps_copy_site_1, 1)
+
+    ρ1 = prime(mps_copy_site_1[1], sites[1]) * dag(mps_copy_site_1[1])
+    ρ1 = matrix(ρ1)
+
+    # check traces are equal to 1
+    if !isequal(abs(tr(ρ1)), 1)
+        error("Trace of 1-site RDM at site 1 not equal to 1")
+    end
+    # get proba densities
+    proba_densities_site1 = []
+    for j in eachindex(states)
+        psi = states[j]
+        expect_s1 = abs(psi' * ρ1 * psi)^2
+        push!(proba_densities_site1, expect_s1)
+    end
+    # normalise 
+    proba_densities_site1 ./= sum(proba_densities_site1)
+    # check 
+    if !isequal(sum(proba_densities_site1), 1)
+        error("Site 1 distribution not normalized!")
+    end
+    cdf_site1 = cumsum(proba_densities_site1)
+    # now sample
+    for i in eachindex(samples)
+        # start with site 1
+        r = rand()
+        k1 = findlast(x -> x <= r, cdf_site1)
+        #println("Sampled state: $(states[k1]) -> x = $(xs[k1])")
+        samples[i, 1] = xs[k1]
+        # now construct projector and project mps into subspace
+        sampled_state = xs[k1]
+        site_1_projector = sampled_state * sampled_state'
+        # make into a one site MPO 
+        site_1_proj_op = op(site_1_projector, sites[1])
+        # apply to the mps at site 1
+        site_1_old = deepcopy(mps[1])
+        site_1_new = site_1_old * site_1_proj_op
+        noprime!(site_1_new)
+        # add back into the mps and normalise
+        mps[1] = site_1_new
+        normalize!(mps)
+        # now sample site 2, conditioned on first sample
+        mps_copy_site_2 = deepcopy(mps)
+        orthogonalize!(mps_copy_site_2, 2)
+        ρ2 = prime(mps_copy_site_2[2], sites[2]) * dag(mps_copy_site_1[2])
+        ρ2 = matrix(ρ2)
+        # do checks
+        if !isequal(abs(tr(ρ2)), 1)
+            error("Trace of 1 site RDM at site 2 not equal to 1.")
+        end
+
+        proba_densities_site2 = []
+        for j in eachindex(states)
+            psi = states[j]
+            expect_s2 = abs(psi' * ρ2 * psi)^2
+            push!(proba_densities_site2, expect_s2)
+        end
+        # normalise 
+        proba_densities_site2 ./= sum(proba_densities_site2)
+        # check 
+        if !isequal(sum(proba_densities_site2), 1)
+            error("Site 1 distribution not normalized!")
+        end
+        cdf_site2 = cumsum(proba_densities_site2)
+        r = rand()
+        k2 = findlast(x -> x <= r, cdf_site2)
+        #println("Sampled state: $(states[k2]) -> x = $(xs[k2])")
+        samples[i, 2] = xs[k2]
+
+        println("Sample $(i): [$(xs[k1]), $(xs[k2])]")
+    end
+
+end
+# sampling
+# mps_copy = deepcopy(mps)
+# orthogonalize!(mps_copy, 1)
+# ρ1 = prime(mps_copy[1], sites[1]) * dag(mps_copy[1])
+# ρ1 = matrix(ρ1)
+# # consruct prob interval
+# xs = collect(0:0.01:1)
+# states = [complex_feature_map(x) for x in xs]
+# proba_densities = []
+# for i in eachindex(states)
+#     psi = states[i]
+#     expect = abs(psi' * ρ1 * psi)^2
+#     push!(proba_densities, expect)
+# end
+# proba_densities_norm = proba_densities ./ sum(proba_densities)
+# r = rand() # generate uniform random value
+# cdf = cumsum(proba_densities_norm)
+# k = findlast(x -> x <= r, cdf)
+# println("Sampled state: $(states[k]) -> x = $(xs[k])")
+# # construct projector
+# sampled_state = states[k]
+# projector_site_1 = sampled_state * sampled_state'
+# projector_site_1_op = op(projector_site_1, sites[1])
+# # apply to mps 
+# site_1_old = deepcopy(mps[1])
+# site_1_new = site_1_old * projector_site_1_op
+# noprime!(site_1_new)
+# mps[1] = site_1_new
+# normalize!(mps)
+
+# # site 2
+# mps_copy2 = deepcopy(mps)
+# orthogonalize!(mps_copy2, 2)
+# ρ2 = prime(mps_copy2[2], sites[2]) * dag(mps_copy2[2])
+# ρ2 = matrix(ρ2)
+# proba_densities2 = []
+# for i in eachindex(states)
+#     psi = states[i]
+#     expect = abs(psi' * ρ2 * psi)^2
+#     push!(proba_densities2, expect)
+# end
+# proba_densities_norm2 = proba_densities2 ./ sum(proba_densities2)
+# r2 = rand() # generate uniform random value
+# cdf2 = cumsum(proba_densities_norm2)
+# k2 = findlast(x -> x <= r2, cdf2)
+# println("Sampled state: $(states[k2]) -> x = $(xs[k2])")
+
+# println("Sampled state (x1, x2): ($(xs[k]), $(xs[k2]))")
 
