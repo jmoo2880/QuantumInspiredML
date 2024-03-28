b41542043cdb4628e0d72253d14c5459196fe54c
Hugo Stackhouse <hugo.stackhouse@sydney.edu.au>
2024-03-28 12:39:45 +1100

    attempt at getting label index to work (still broken)

--- MPS_MSE/utils.jl
+++ MPS_MSE/utils.jl
@@ -1,18 +1,116 @@
-using ITensors
-using OptimKit
-using Random
-using Distributions
-using DelimitedFiles
-using Folds
-using JLD2
 using StatsBase
+using Random
 using Plots
+using DelimitedFiles
 
 
+
+function AngleEncoder(x::Float64) 
+    """Function to convert normalised time series to an angle encoding."""
+    @assert x <= 1.0 && x >= 0.0 "Data points must be rescaled between 1 and 0 before encoding using the angle encoder."
+    s1 = exp(1im * (3π/2) * x) * cospi(0.5 * x)
+    s2 = exp(-1im * (2π/2) * x) * sinpi(0.5 * x)
+    return [s1, s2]
+ 
+end
+
+function NormalisedDataToProductState(sample::Vector, site_indices::Vector{Index{Int64}})
+    """Function to convert a single normalised sample to a product state
+    with local dimension 2, as specified by the feature map."""
+
+    n_sites = length(site_indices) # number of mps sites
+    product_state = MPS(ComplexF64,site_indices; linkdims=1)
+    
+    # check that the number of sites matches the length of the time series
+    if n_sites !== length(sample)
+        error("Number of MPS sites: $n_sites does not match the time series length: $(length(sample))")
+    end
+
+    for j=1:n_sites
+        T = ITensor(site_indices[j])
+        # map 0 to |0> and 1 to |1> 
+        zero_state, one_state = AngleEncoder(sample[j])
+        T[1] = zero_state
+        T[2] = one_state
+        product_state[j] = T
+    end
+
+    return product_state
+
+end
+
+function GenerateAllProductStates(X_normalised::Matrix, y::Vector{Int}, type::String, 
+    site_indices::Vector{Index{Int64}})
+    """"Convert an entire dataset of normalised time series to a corresponding 
+    dataset of product states"""
+    # check data is in the expected range first
+    if all((0 .<= X_normalised) .& (X_normalised .<= 1)) == false
+        error("Data must be rescaled between 0 and 1 before generating product states.")
+    end
+
+    types = ["train", "test", "valid"]
+    if type in types
+        println("Initialising $type states.")
+    else
+        error("Invalid dataset type. Must be train, test, or valid.")
+    end
+
+    num_samples = size(X_normalised)[1]
+    # pre-allocate
+    all_product_states = timeSeriesIterable(undef, num_samples)
+
+    for i=1:num_samples
+        sample_pstate = NormalisedDataToProductState(X_normalised[i, :], site_indices)
+        sample_label = y[i]
+        product_state = PState(sample_pstate, sample_label, type)
+        all_product_states[i] = product_state
+    end
+
+    return all_product_states
+
+end;
+
+function LoadSplitsFromTextFile(train_set_location::String, val_set_location::String, 
+    test_set_location::String)
+    """As per typical UCR formatting, assume labels in first column, followed by data"""
+    # do checks
+    train_data = readdlm(train_set_location)
+    val_data = readdlm(val_set_location)
+    test_data = readdlm(test_set_location)
+
+    X_train = train_data[:, 2:end]
+    y_train = Int.(train_data[:, 1])
+
+    X_val = val_data[:, 2:end]
+    y_val = Int.(val_data[:, 1])
+
+    X_test = test_data[:, 2:end]
+    y_test = Int.(test_data[:, 1])
+
+    # recombine val and train into train
+
+    return (X_train, y_train), (X_val, y_val), (X_test, y_test)
+
+end
+
+function generate_training_data(samples_per_class::Int, data_pts::Int=5)
+
+    class_A_samples = zeros(samples_per_class, data_pts)
+    class_B_samples = ones(samples_per_class, data_pts)
+    all_samples = vcat(class_A_samples, class_B_samples)
+    all_labels = Int.(vcat(zeros(size(class_A_samples)[1]), ones(size(class_B_samples)[1])))
+
+    shuffle_idxs = shuffle(1:samples_per_class*2)
+
+
+    return all_samples[shuffle_idxs, :], all_labels[shuffle_idxs]
+
+end
 
 function GenerateSine(n, amplitude=1.0, frequency=1.0)
     t = range(0, 2π, n)
     phase = rand(Uniform(0, 2π)) # randomise the phase
+    #amplitude = rand(Uniform(0.1, 1.0))
     return amplitude .* sin.(frequency .* t .+ phase) .+ 0.2 .* randn(n)
 end
 
@@ -20,18 +118,18 @@ function GenerateRandomNoise(n, scale=1)
     return randn(n) .* scale
 end
 
-function GenerateToyDataset(n, dataset_size, train_split=0.7, val_split=0.15)
+function GenerateToyDataset(n, dataset_size, train_split=0.7)
     # calculate size of the splits
     train_size = floor(Int, dataset_size * train_split) # round to an integer
-    val_size = floor(Int, dataset_size * val_split) # do the same for the validation set
-    test_size = dataset_size - train_size - val_size # whatever remains
+    #val_size = floor(Int, dataset_size * val_split) # do the same for the validation set
+    test_size = dataset_size - train_size
 
     # initialise structures for the datasets
     X_train = zeros(Float64, train_size, n)
     y_train = zeros(Int, train_size)
 
-    X_val = zeros(Float64, val_size, n)
-    y_val = zeros(Int, val_size)
+    #X_val = zeros(Float64, val_size, n)
+    #y_val = zeros(Int, val_size)
 
     X_test = zeros(Float64, test_size, n)
     y_test = zeros(Int, test_size)
@@ -43,23 +141,23 @@ function GenerateToyDataset(n, dataset_size, train_split=0.7, val_split=0.15)
 
     for i in 1:train_size
         label = rand(0:1)  # Randomly choose between sine wave (0) and noise (1)
-        data = label == 0 ? GenerateSine(n) : GenerateRandomNoise(n)
+        data = label == 0 ? GenerateSine(n, 1.0, 2.0) : GenerateSine(n, 1.0, 5.0)
         insert_data!(X_train, y_train, i, data, label)
     end
 
-    for i in 1:val_size
-        label = rand(0:1)
-        data = label == 0 ? GenerateSine(n) : GenerateRandomNoise(n)
-        insert_data!(X_val, y_val, i, data, label)
-    end
+    # for i in 1:val_size
+    #     label = rand(0:1)
+    #     data = label == 0 ? GenerateSine(n) : GenerateRandomNoise(n)
+    #     insert_data!(X_val, y_val, i, data, label)
+    # end
 
     for i in 1:test_size
         label = rand(0:1)
-        data = label == 0 ? GenerateSine(n) : GenerateRandomNoise(n)
+        data = label == 0 ? GenerateSine(n, 1.0, 2.0) : GenerateSine(n, 1.0, 5.0)
         insert_data!(X_test, y_test, i, data, label)
     end
 
-    return (X_train, y_train), (X_val, y_val), (X_test, y_test)
+    return (X_train, y_train), (X_test, y_test)
 
 end
 
@@ -119,166 +217,53 @@ function PlotTrainingSummary(info::Dict)
 
 end
 
-function LoadSplitsFromTextFile(train_set_location::String, val_set_location::String, 
-    test_set_location::String)
-    """As per typical UCR formatting, assume labels in first column, followed by data"""
-    # do checks
-    train_data = readdlm(train_set_location)
-    val_data = readdlm(val_set_location)
-    test_data = readdlm(test_set_location)
+struct RobustSigmoidTransform{T<:Real} <: AbstractDataTransform
+    median::T
+    iqr::T
+    k::T
+    positive::Bool
 
-    X_train = train_data[:, 2:end]
-    y_train = Int.(train_data[:, 1])
-
-    X_val = val_data[:, 2:end]
-    y_val = Int.(val_data[:, 1])
-
-    X_test = test_data[:, 2:end]
-    y_test = Int.(test_data[:, 1])
-
-    return (X_train, y_train), (X_val, y_val), (X_test, y_test)
-
+    function RobustSigmoidTransform(median::T, iqr::T, k::T, positive=true) where T<:Real
+        new{T}(median, iqr, k, positive)
+    end
 end
 
-function SliceMPS(W::MPS)
-    """Gets the label index of the MPS and slices according to the number of classes (dim of the label index)"""
-    """Assume one-hot encoding scheme i.e. class 0 = [1, 0], class 1 = [0, 1], etc. """
-    dec_index = findindex(W[end], "f(x)")
-    if dec_index == nothing
-        error("Label index not found on the first site of the MPS!")
+function robust_sigmoid(x::Real, median::Real, iqr::Real, k::Real, positive::Bool)
+    xhat = 1.0 / (1.0 + exp(-(x - median) / (iqr / k)))
+    if !positive
+        xhat = 2*xhat - 1
     end
+    return xhat
+end
 
-    n_states = ITensors.dim(dec_index)
-    states = []
-    for i=1:n_states
-        state = deepcopy(W)
-        if !isapprox(norm(state), 0)
-            normalize!(state)
-        end
-        decision_state = onehot(dec_index => (i))
-        println("Class $(i-1) state: $(vector(decision_state))")
-        state[end] *= decision_state
-        normalize!(state)
-        push!(states, state)
+function fitScaler(::Type{RobustSigmoidTransform}, X::Matrix; k::Real=1.35, positive::Bool=true)
+    medianX = median(X)
+    iqrX = iqr(X)
+    return RobustSigmoidTransform(medianX, iqrX, k, positive)
+end
+
+function transformData(t::RobustSigmoidTransform, X::Matrix)
+    return map(x -> robust_sigmoid(x, t.median, t.iqr, t.k, t.positive), X)
+end
+
+# New SigmoidTransform
+struct SigmoidTransform <: AbstractDataTransform
+    positive::Bool
+end
+
+function sigmoid(x::Real, positive::Bool)
+    xhat = 1.0 / (1.0 + exp(-x))
+    if !positive
+        xhat = 2*xhat - 1
     end
+    return xhat
+end
 
-    return states
+function fitScaler(::Type{SigmoidTransform}, X::Matrix; positive::Bool=true)
+    return SigmoidTransform(positive)
+end
+
+function transformData(t::SigmoidTransform, X::Matrix)
+    return map(x -> sigmoid(x, t.positive), X)
 end;
 
-function entropy_von_neumann(ψ::MPS, b::Int)
-    psi = deepcopy(ψ)
-    s = siteinds(psi)
-    orthogonalize!(psi, b) # change orthogonality center to site B
-    #print(norm(psi))
-    if b == 1
-        _, S = svd(psi[b], (siteind(psi, b),))
-    else
-        _, S = svd(psi[b], (linkind(psi, b-1), s[b]))
-    end
-    SvN = 0.0
-    for n in 1:ITensors.dim(S, 1)
-        p = S[n, n]^2
-        if p > 1E-12
-            SvN -= p * log(p)
-        end
-    end
-
-    return SvN
-end;
-
-function LoadMPS(fname::String)
-    """Function to load a trained MPS"""
-    f = h5open(fname, "r")
-    W = read(f, "mps", MPS)
-
-    return W
-
-end
-
-function SampleMPS(W::MPS; num_discrete_pts=100)
-    """Function to sample from a MPS.
-    Takes in an MPS corresponding to a state that overlaps with a given
-    class (no label index attached)."""
-
-    # grid the angle range 
-    theta_range = collect(range(0, stop=pi/2, length=num_discrete_pts));
-    states = [[cos(theta), sin(theta)] for theta in theta_range];
-
-    mps = deepcopy(W)
-    sites = siteinds(mps)
-    num_sites = length(W)
-    sampled_angles = Vector{Float64}(undef, num_sites)
-
-    for i=1:num_sites
-        orthogonalize!(mps, i) # shift orthogonality center to site i
-        ρ = prime(mps[i], sites[i]) * dag(mps[i])
-        ρ = matrix(ρ)
-        # checks on the rdm
-        if !isapprox(tr(ρ), 1)
-            error("Reduced density matrix at site $i does not have a tr(ρ) ≈ 1")
-        end
-
-        probas = Vector{Float64}(undef, num_discrete_pts)
-        for j=1:length(states)
-            psi = states[j]
-            expect = psi' * ρ * psi
-            probas[j] = expect
-        end
-
-        # normalise the probabilities
-        probas_normalised = probas ./ sum(probas)
-        # ensure normalised
-        if !isapprox(sum(probas_normalised), 1)
-            error("Probabilities not normalised!")
-        end
-
-        # create a categorical distribution with the normalised probabilities
-        dist = Categorical(probas_normalised)
-        sampled_index = rand(dist)
-    
-        sampled_angles[i] = theta_range[sampled_index]
-
-        # now make the measurement at the site
-        sampled_state = states[sampled_index]
-
-        # make the projector |x><x|
-        site_projector_matrix = sampled_state * sampled_state'
-        site_projector_operator = op(site_projector_matrix, sites[i])
-        site_before_measure = deepcopy(mps[i])
-
-        # apply single site mpo
-        site_after_measure = site_before_measure * site_projector_operator
-        noprime!(site_after_measure)
-
-        # add measured site back into the mps
-        mps[i] = site_after_measure
-        normalize!(mps)
-    end
-
-    # convert angles back to time-series values
-
-    sampled_time_series_values = (2 .* sampled_angles) ./ π
-
-    return sampled_time_series_values
-
-end
-
-function MPSMonteCarlo(W::MPS, num_trials::Int; num_discrete_pts::Int=100)
-
-    all_samples = Matrix{Float64}(undef, num_trials, length(W))
-
-    # each threads gets its own local RNG to ensure no correlation between the RN in each thread
-    #rngs = [MersenneTwister(i) for i in 1: Threads.nthreads()];
-    
-    Threads.@threads for trial=1:num_trials
-        
-        s = SampleMPS(W; num_discrete_pts=num_discrete_pts)
-        all_samples[trial, :] = s
-    end
-
-    mean_values = mean(all_samples, dims=1)
-    std_values = std(all_samples, dims=1)
-
-    return mean_values, std_values
-        
-end
