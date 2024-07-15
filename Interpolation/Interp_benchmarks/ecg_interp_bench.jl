43b0118747cf371f37f7f31dda655df69d941d82
Hugo Stackhouse <hugo.stackhouse@sydney.edu.au>
2024-07-15 21:32:44 +1000

    trying to find scenarios with some improvement; added more benchmarks

---
+++ Interpolation/Interp_benchmarks/ecg_interp_bench.jl
@@ -0,0 +1,77 @@
+include("../../LogLoss/RealRealHighDimension.jl")
+include("../ForecastingMainNew.jl");
+using JLD2
+using DataFrames
+using StatsBase
+
+function midpoint_benchmark(ys_test, fc, interp_sites, Xs_train, ys_train)
+    n1s = sum(ys_test)
+    n0s = length(ys_test) - n1s
+
+
+    samples = [shuffle(1:n0s); shuffle(1:n1s)]
+    classes = [zeros(Int,n0s); ones(Int,n1s)]
+
+    ps = []
+    stats = []
+    MSE_stats = []
+    for (i,s) in enumerate(samples)
+        class = classes[i]
+        instance_idx = s
+        stat, MSE_stat, p1 = any_interpolate_single_time_series(fc, class, instance_idx, interp_sites, :directMode; MSE_baseline=true, X_train_scaled=Xs_train, y_train=ys_train);
+        push!(ps, p1)
+        push!(stats, stat)
+        push!(MSE_stats, MSE_stat)
+    end
+
+    return stats, MSE_stats, ps
+end
+
+function make_interp_sites(n_mid::Integer, Xs::Matrix{Float64})
+    npoints = size(Xs, 2)
+    start_data = ceil(Int,(npoints+1) / 2 - n_mid/2)
+
+    return [1:(start_data-1); (start_data + n_mid):npoints]
+end
+
+
+
+svpath = "Interpolation/paper/ecg200/mps_saves/legendreNN2_d8_chi35.jld2"
+
+f = jldopen(svpath, "r")
+    X_train_scaled = read(f, "X_train_scaled")
+    y_train = read(f, "y_train")
+    X_test_scaled = read(f, "X_test_scaled")
+    y_test = read(f, "y_test");
+    mps = read(f, "mps")
+    opts = read(f, "opts")
+close(f)
+
+fc = load_forecasting_info_variables(mps, X_train_scaled, y_train, X_test_scaled, y_test, opts);
+
+
+n_midpoints = [1,2]#collect(1:2:50)
+
+stats = Vector{Any}(undef, length(n_midpoints))
+MSE_stats = Vector{Any}(undef, length(n_midpoints))
+ps = Vector{Any}(undef, length(n_midpoints))
+
+for i in eachindex(n_midpoints)
+    n_mid = n_midpoints[i]
+    interp_sites = make_interp_sites(n_mid, X_train_scaled)
+    stats[i], MSE_stats[i], ps[i] = midpoint_benchmark(y_test, fc, interp_sites, X_train_scaled, y_train)
+    
+end
+
+idstr = "ecg_"*prod(map(x-> string(x) * "_", n_midpoints))[1:end-1] *".jld2"
+
+path = "Interpolation/Interp_benchmarks/midpoint_benchmarks/" * idstr
+
+jldopen(path, "w") do f
+    f["n_midpoints"] = n_midpoints
+    f["stats"] = stats
+    f["MSE_stats"] = MSE_stats
+    f["ps"] = ps
+end
+
+
